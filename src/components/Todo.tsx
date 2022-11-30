7bb7cb5a0be0677b48ec7e0e869734ebf50d9c4a
석주 <54101187+sbbtt@users.noreply.github.com>
2022-11-30 17:19:11 +0900

    minor
    TS의 엘리먼트 이벤트 타입명시

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,26 @@
-import { ITodo } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { ITodo, toDoState } from "./atoms";
 
-function Todo({text}:ITodo){
-    return <li>{text} 
-    <button>To Do</button> 
-    <button>Doing</button> 
-    <button>Done</button> 
-    </li>;
+function Todo({text, category, id}:ITodo){
+    const setTodos = useSetRecoilState(toDoState)
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
+        const {currentTarget:{name}}= event
+    }
+    return (
+      <li>
+        <span>{text}</span>
+        {category !== "TO_DO" && (
+          <button name='todo' onClick={onClick}>To do</button >
+        )}
+        {category !== "DOING" && (
+          <button name='doing' onClick={onClick}>Doing</button>
+        )}
+        {category !== "DONE" && (
+          <button name='done' onClick={onClick}>Done</button>
+        )}
+      </li>
+    );
     
 }
 
